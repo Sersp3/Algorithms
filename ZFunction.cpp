a12f0d2694984ffd9cdbf020f0a17a2ff507d107
Sergey <pavlikov.ss@phystech.edu>
2026-04-14 11:17:00 +0300

    Modified ZFunction

--- ZFunction.cpp
+++ ZFunction.cpp
@@ -7,16 +7,9 @@ std::vector<int> ZFunction(const std::string& str) {
   std::vector<int> z(n, 0);
   int l = 0, r = 0;
   for (int i = 1; i < n; ++i) {
-    if (r >= i) {
-      z[i] = std::min(r - i + 1, z[i - l]);
-    }
-
+    if (r >= i) z[i] = std::min(r - i + 1, z[i - l]);
     while (z[i] + i < n && str[z[i] + i] == str[z[i]]) ++z[i];
-
-    if (z[i] + i - 1 > r) {
-      l = i;
-      r = z[i] + i - 1;
-    }
+    if (z[i] + i - 1 > r) l = i, r = z[i] + i - 1;
   }
   return z;
 }
